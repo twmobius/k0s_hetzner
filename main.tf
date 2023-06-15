9c8298889130f325bc5bd5b5739825376067a770
Alexandros Kosiaris <akosiaris@gmail.com>
2023-06-15 17:23:14 +0300

    terraform fmt everything
    Need to put a check in GH action for this
    
    Signed-off-by: Alexandros Kosiaris <akosiaris@gmail.com>

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ provider "hcloud" {
 
 # Create a new server running debian
 resource "hcloud_server" "controller1" {
-  name        = "controller1"
+  name = "controller1"
   # arm64 machine
   server_type = "cax11"
   # TODO: Bump to 12 once it's available
-  image       = "debian-11"
+  image = "debian-11"
   # Only falkenstein has arm64 for now
-  location    = "fsn1"
+  location  = "fsn1"
   user_data = file("user-data")
   public_net {
     ipv4_enabled = true
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "hcloud_token" {
-  sensitive = true # Requires terraform >= 0.14
+  sensitive   = true # Requires terraform >= 0.14
   description = "Value of the Hetzner token"
-  type = string
+  type        = string
 }
