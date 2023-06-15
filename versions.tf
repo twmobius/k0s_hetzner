060056ada3aa0b21073af74431ba902a0a3d407b
Alexandros Kosiaris <akosiaris@gmail.com>
2023-06-15 10:20:21 +0300

    Add the basic versioned provider
    Add in versions.tf the terraform provider
    
    Signed-off-by: Alexandros Kosiaris <akosiaris@gmail.com>

---
+++ versions.tf
@@ -0,0 +1,9 @@
+# Docs at https://registry.terraform.io/providers/hetznercloud/hcloud/latest
+terraform {
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "1.40.0"
+    }
+  }
+}
