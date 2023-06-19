951003a59ba16aadfff8d7abeaf8da3bfdd6a6f9
Alexandros Kosiaris <akosiaris@gmail.com>
2023-06-19 11:21:24 +0300

    Fix a minor typo
    s/controller1/controller/
    
    Signed-off-by: Alexandros Kosiaris <akosiaris@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "controller_ip4_addr" {
   value = hcloud_server.controller.*.ipv4_address
 }
 
-output "controller1_ip6_addr" {
+output "controller_ip6_addr" {
   value = hcloud_server.controller.*.ipv6_address
 }
