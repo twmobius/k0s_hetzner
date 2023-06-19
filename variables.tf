79ede52583d7c5906761b735be598716fcc0fcf6
Alexandros Kosiaris <akosiaris@gmail.com>
2023-06-19 16:40:35 +0300

    Fix k0s version condition
    Need to escape the + and . characters as they have special meaning in
    regexps and we want to be pretty specific right now. In hindsight, maybe
    we shouldn't be using rexeps to begin with at this stage, but hopefully
    this will soon be targetting more than 1 version
    
    Signed-off-by: Alexandros Kosiaris <akosiaris@gmail.com>

--- variables.tf
+++ variables.tf
@@ -64,7 +64,7 @@ variable "k0s_version" {
   description = "The version of k0s to target. Default: 1.27.2+k0s.0"
   default     = "1.27.2+k0s.0"
   validation {
-    condition     = can(regex("1.27.2+k0s.0", var.k0s_version))
+    condition     = can(regex("1.27.2\\+k0s\\.0", var.k0s_version))
     error_message = "Unsupported k0s version provided"
   }
 }
