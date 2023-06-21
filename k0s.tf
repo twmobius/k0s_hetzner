824d179dd967986047eb52733b0fb4528b0200b8
Alexandros Kosiaris <akosiaris@gmail.com>
2023-06-21 20:12:07 +0300

    Move k0s stuff into dedicated file
    Split off, should make it a bit easier for me to dev moar
    
    Signed-off-by: Alexandros Kosiaris <akosiaris@gmail.com>

---
+++ k0s.tf
@@ -0,0 +1,41 @@
+# k0s resource to create a cluster and store a kubeconfig file
+resource "k0s_cluster" "k0s1" {
+  name    = var.domain
+  version = var.k0s_version
+
+  hosts = concat(
+    [
+      for address in hcloud_server.controller.*.ipv4_address :
+      {
+        role        = var.controller_role
+        no_taints   = var.controller_role == "controller+worker" ? true : false
+        environment = { "ROLE" = var.controller_role }
+        ssh = {
+          address  = address
+          port     = 22
+          user     = "root"
+          key_path = var.ssh_priv_key_path
+        }
+      }
+    ],
+    [
+      for address in hcloud_server.worker.*.ipv4_address :
+      {
+        role        = "worker"
+        environment = { "ROLE" = "worker" }
+        ssh = {
+          address  = address
+          port     = 22
+          user     = "root"
+          key_path = var.ssh_priv_key_path
+        }
+      }
+    ]
+  )
+}
+
+resource "local_file" "kubeconfig" {
+  filename        = "kubeconfig-${var.domain}"
+  file_permission = "0600"
+  content         = nonsensitive(k0s_cluster.k0s1.kubeconfig)
+}
