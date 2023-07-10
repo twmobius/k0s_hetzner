b6340411c012de93c01ab0cae1ccfcb69b297645
Alexandros Kosiaris <akosiaris@gmail.com>
2023-07-10 09:49:03 +0300

    Spawn controllers too using the new modules
    Now that we apparrently got worker ready, let's add controllers too
    
    Signed-off-by: Alexandros Kosiaris <akosiaris@gmail.com>

--- main.tf
+++ main.tf
@@ -16,12 +16,20 @@ module "worker_ips" {
   domain = var.domain
 }
 
+module "controller_ips" {
+  source = "./primary_ip"
+
+  amount = var.controller_count
+  role   = "controller"
+  domain = var.domain
+}
+
 module "workers" {
   source = "./server"
 
   amount            = var.worker_count
   role              = "worker"
-  ssh_pub_key_id   = hcloud_ssh_key.default.id 
+  ssh_pub_key_id    = hcloud_ssh_key.default.id
   ssh_priv_key_path = var.ssh_priv_key_path
   domain            = var.domain
   ip_addresses      = module.worker_ips.addresses
@@ -30,7 +38,7 @@ module "workers" {
       proto = "tcp",
       ports = [179],
       cidrs = concat(
-        module.worker_ips.addresses["ipv4"],
+        module.worker_ips.addresses["ipv6"],
         module.worker_ips.addresses["ipv4"],
       )
     }
@@ -38,7 +46,7 @@ module "workers" {
       proto = "udp",
       ports = [4789],
       cidrs = concat(
-        module.worker_ips.addresses["ipv4"],
+        module.worker_ips.addresses["ipv6"],
         module.worker_ips.addresses["ipv4"],
       )
     }
@@ -46,7 +54,7 @@ module "workers" {
       proto = "tcp",
       ports = [10250],
       cidrs = concat(
-        module.worker_ips.addresses["ipv4"],
+        module.worker_ips.addresses["ipv6"],
         module.worker_ips.addresses["ipv4"],
       )
     }
@@ -54,7 +62,7 @@ module "workers" {
       proto = "tcp",
       ports = [10249],
       cidrs = concat(
-        module.worker_ips.addresses["ipv4"],
+        module.worker_ips.addresses["ipv6"],
         module.worker_ips.addresses["ipv4"],
       )
     }
@@ -62,8 +70,57 @@ module "workers" {
       proto = "tcp",
       ports = [9100],
       cidrs = concat(
+        module.worker_ips.addresses["ipv6"],
         module.worker_ips.addresses["ipv4"],
+      )
+    }
+  }
+}
+
+module "controllers" {
+  source = "./server"
+
+  amount            = var.controller_count
+  role              = "controller"
+  ssh_pub_key_id    = hcloud_ssh_key.default.id
+  ssh_priv_key_path = var.ssh_priv_key_path
+  domain            = var.domain
+  ip_addresses      = module.controller_ips.addresses
+  firewall_rules = {
+    k8s-api = {
+      proto = "tcp",
+      ports = [6443],
+      cidrs = concat(
+        module.worker_ips.addresses["ipv6"],
         module.worker_ips.addresses["ipv4"],
+        module.controller_ips.addresses["ipv6"],
+        module.controller_ips.addresses["ipv4"],
+      )
+    }
+    etcd = {
+      proto = "tcp",
+      ports = [2380],
+      cidrs = concat(
+        module.controller_ips.addresses["ipv6"],
+        module.controller_ips.addresses["ipv4"],
+      )
+    }
+    konnectivity = {
+      proto = "tcp",
+      ports = [8132, 8133],
+      cidrs = concat(
+        module.worker_ips.addresses["ipv6"],
+        module.worker_ips.addresses["ipv4"],
+      )
+    }
+    k0s-api = {
+      proto = "tcp",
+      ports = [9443],
+      cidrs = concat(
+        module.worker_ips.addresses["ipv6"],
+        module.worker_ips.addresses["ipv4"],
+        module.controller_ips.addresses["ipv6"],
+        module.controller_ips.addresses["ipv4"],
       )
     }
   }
